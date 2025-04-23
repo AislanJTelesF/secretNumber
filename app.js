a15096c4adcbe2d7b499c8160533f868b2aa9676
AislanJTelesF <aislanjesus2019@gmail.com>
2025-04-23 10:18:22 -0300

    Deixando o jogo mais fácil

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function showInitialMessage() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 showInitialMessage();
@@ -78,7 +78,7 @@ function verificarChute() {
 }
 
 function randomNumber() {
-    return parseInt(Math.random() * 1000 + 1);
+    return parseInt(Math.random() * 50 + 1);
 }
 
 function clearSpace() {
