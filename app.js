a28651d7180116db11af1179f56c38e0480723f2
AislanJTelesF <aislanjesus2019@gmail.com>
2025-04-23 09:00:18 -0300

    mudar limite máximo

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ function verificarChute() {
 }
 
 function randomNumber() {
-    return parseInt(Math.random() * 100 + 1);
+    return parseInt(Math.random() * 1000 + 1);
 }
 
 function clearSpace() {
