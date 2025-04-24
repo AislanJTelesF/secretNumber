5271231747dd48c6fe55c72a5e816f9c1363a535
AislanJTelesF <aislanjesus2019@gmail.com>
2025-04-24 11:58:30 -0300

    etapa 4

--- app.js
+++ app.js
@@ -43,6 +43,60 @@
 
 // Etapa 3
 
+// let secretNumber = randomNumber();
+// let guesses = 1;
+
+// function exibirTextoNaTela(tag, texto) {
+//     let campo = document.querySelector(tag);
+//     campo.innerHTML = texto;
+// }
+
+// function showInitialMessage() {
+//     exibirTextoNaTela('h1', 'Jogo do número secreto');
+//     exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+// }
+
+// showInitialMessage();
+
+// function verificarChute() {
+//     let guess = document.querySelector('input').value;
+//     if (guess == secretNumber) {
+//         exibirTextoNaTela('h1', 'Acertou!');
+//         let wordGuesses = guesses > 1? 'tentativas' : 'tentativa';
+//         let messagesGuesses = `Você descobriu o número secreto com ${guesses} ${wordGuesses}!`;
+//         exibirTextoNaTela('p', messagesGuesses);
+//         document.getElementById('reiniciar').removeAttribute('disabled');
+//     } else {
+//         if (guess > secretNumber) {
+//             exibirTextoNaTela('p', 'O número secreto é menor'); 
+//         } else {
+//             exibirTextoNaTela('p', 'O número secreto é maior');
+//         }
+//         guesses++;
+//         clearSpace();
+//     }
+// }
+
+// function randomNumber() {
+//     return parseInt(Math.random() * 100 + 1);
+// }
+
+// function clearSpace() {
+//     guess = document.querySelector('input');
+//     guess.value = '';
+// }
+
+// function restartGame() {
+//     secretNumber = randomNumber();
+//     clearSpace();
+//     guesses = 1;
+//     showInitialMessage();
+//     document.getElementById('reiniciar').setAttribute('disabled', true);
+// }
+
+// Etapa 4
+
+let arrayOfRandomNumbers = [];
 let secretNumber = randomNumber();
 let guesses = 1;
 
@@ -78,7 +132,14 @@ function verificarChute() {
 }
 
 function randomNumber() {
-    return parseInt(Math.random() * 100 + 1);
+    let chooseNumber = parseInt(Math.random() * 4 + 1);
+    if (arrayOfRandomNumbers.includes(chooseNumber)) {
+        return randomNumber();
+    } else {
+        arrayOfRandomNumbers.push(chooseNumber);
+        console.log(arrayOfRandomNumbers);
+        return chooseNumber;
+    }
 }
 
 function clearSpace() {
